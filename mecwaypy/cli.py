0a3052f1ec105c5d4786d1bb1a51ce61ac5155ad
Rick Teachey <ricky@teachey.org>
2019-12-04 15:16:34 -0500

    started cli, test_cli -- actual click CLI not yet written

---
+++ mecwaypy/cli.py
@@ -0,0 +1,54 @@
+from multiprocessing.pool import AsyncResult
+from pathlib import Path
+import subprocess as sp
+from multiprocessing import Pool
+import os
+from typing import Iterable, Optional, Union, Iterator
+from mecwaypy.mecway import MecwayException
+
+
+class SolveException(MecwayException):
+    pass
+
+
+MECWAY_EXE_REL = r"Mecway\Mecway8\mecway.exe"
+
+try:
+    MECWAY_EXE_PATH = (Path(os.environ["PROGRAMFILES"]) / MECWAY_EXE_REL).resolve(strict=True)
+except FileNotFoundError as e:
+    raise SolveException("mecway executable not found") from e
+except KeyError as e:
+    raise SolveException("Program Files environment variable not found") from e
+
+
+def solve(liml_path: Union[Path, str], window:bool=False, **kwargs) -> sp.CompletedProcess:
+    args = [f'"{MECWAY_EXE_PATH!s}"', f'"{liml_path!s}"']
+    if not window:
+        args.append("solve")
+    result = sp.run(args, **kwargs)
+    return result
+
+
+def solve_multi(path_or_paths: Union[Path, str, Iterable[Union[Path, str]]]=(), glob: Optional[str]=None, n: int=2, **kwargs):
+    # get an iterable of Path objects
+    paths = (Path(path_or_paths),) if isinstance(path_or_paths, (Path, str)) else (Path(p) for p in path_or_paths)
+
+    # get a Path object iterator
+    iter_paths: Iterator[Path]
+    if glob:
+        iter_paths = (f for p in paths for f in Path(p).glob(glob))
+    else:
+        iter_paths = iter(paths)
+
+    pool = Pool(processes=n)
+    for p in iter_paths:
+        if p.suffix!=".liml":
+            raise SolveException(f"invalid file type: {p.suffix}")
+        pool.apply_async(solve, kwds=kwargs, callback=handle_result)
+    pool.close()
+    pool.join()
+
+
+def handle_result(result: AsyncResult) -> None:
+    result.successful()
+    print(result.get())
---
+++ tests/test_cli.py
@@ -0,0 +1,73 @@
+import pytest
+from mecwaypy.cli import solve_multi, handle_result, solve, MECWAY_EXE_PATH
+from multiprocessing.pool import Pool
+from pathlib import Path
+import subprocess
+
+
+@pytest.fixture
+def apply_async_result():
+    return []
+
+
+@pytest.fixture
+def run_result():
+    return []
+
+
+@pytest.fixture
+def mock_appy_async(apply_async_result):
+    def apply_async(self, solve, kwds, callback):
+        apply_async_result.extend([solve, kwds, callback])
+    return apply_async
+
+
+@pytest.fixture
+def mock_glob():
+    def glob(*args, **kwargs):
+        yield Path("foo.liml")
+    return glob
+
+
+@pytest.fixture
+def mock_run(run_result):
+    def run(args, **kwargs):
+        run_result.extend([args, kwargs])
+    return run
+
+
+@pytest.fixture
+def patched_apply_async(monkeypatch, mock_appy_async):
+    monkeypatch.setattr(Pool, "apply_async", mock_appy_async)
+
+
+@pytest.fixture
+def patched_glob(monkeypatch, mock_glob):
+    monkeypatch.setattr(Path, "glob", mock_glob)
+
+
+@pytest.fixture
+def patched_run(monkeypatch, mock_run):
+    monkeypatch.setattr(subprocess, "run", mock_run)
+
+
+@pytest.mark.parametrize("path_or_paths, glob", [
+    ("foo.liml", None),
+    (Path("foo.liml"), None),
+    ("", "foo.liml"),
+])
+def test_solve_multi(path_or_paths, glob, patched_apply_async, apply_async_result, patched_glob):
+    kwds = dict(glob=glob, window=True)
+    solve_multi(path_or_paths, **kwds)
+    kwds.pop("glob")
+    assert apply_async_result == [solve, kwds, handle_result]
+
+
+@pytest.mark.parametrize("liml_path", [
+    "foo.liml",
+    Path("foo.liml")
+])
+def test_solve(liml_path, patched_run, run_result):
+    solve(liml_path)
+    args = [f'"{MECWAY_EXE_PATH!s}"', f'"{liml_path!s}"', "solve"]
+    assert run_result == [args, {}]
