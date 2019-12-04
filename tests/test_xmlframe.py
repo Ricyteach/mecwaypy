1539e14490de753baa37459daf85464f037b72a9
Ricky Teachey <ricky@teachey.org>
2019-12-03 23:17:08 -0500

    Blackenned all modules

--- tests/test_xmlframe.py
+++ tests/test_xmlframe.py
@@ -3,32 +3,36 @@ import pandas as pd
 import pytest
 
 
-@pytest.mark.parametrize("txt", [
-    """<q attr1="a" attr2="b">
+@pytest.mark.parametrize(
+    "txt",
+    [
+        """<q attr1="a" attr2="b">
     <p attr3="c"/>
     <v attr4="d"/>
     </q>""",
-
-    """<q>
+        """<q>
     <p/>
     <v/>
     </q>""",
-])
+    ],
+)
 def test_parsed_df(txt):
-    df=pd.DataFrame(txt.split("\n"), columns=['a'])
+    df = pd.DataFrame(txt.split("\n"), columns=["a"])
     parsed_df = _parsed_df(df)
     idxs = range(4)
     tags = "qpvq"
-    assert list(parsed_df[TAG].iteritems())==list(zip(idxs, tags))
+    assert list(parsed_df[TAG].iteritems()) == list(zip(idxs, tags))
     left_brackets = "<", "<", "<", "</"
-    assert list(parsed_df[LB].iteritems())==list(zip(idxs, left_brackets))
+    assert list(parsed_df[LB].iteritems()) == list(zip(idxs, left_brackets))
     right_brackets = ">", "/>", "/>", ">"
-    assert list(parsed_df[RB].iteritems())==list(zip(idxs, right_brackets))
+    assert list(parsed_df[RB].iteritems()) == list(zip(idxs, right_brackets))
     ...
 
 
-@pytest.mark.parametrize("txt", [
-    """<q name="q1" attr1="a" attr2="b">
+@pytest.mark.parametrize(
+    "txt",
+    [
+        """<q name="q1" attr1="a" attr2="b">
     <p attr3="c"/>
     <v attr4="d"/>
     </q>
@@ -36,9 +40,10 @@ def test_parsed_df(txt):
     <p attr3="c"/>
     <v attr4="d"/>
     </q>""",
-
-])
+    ],
+)
 def test_tag_group(txt):
-    df=pd.DataFrame(txt.split("\n"))
+    df = pd.DataFrame(txt.split("\n"))
     parsed_df = _parsed_df(df)
-    assert TagGroup("q", "name", parsed_df)
+    t = TagGroup("q", "name", parsed_df)
+    assert t
